c1fbecea9a75111864c22f0d72a5132aa04adcb6
Akshaya <akshaya.kini12@gmail.com>
2026-01-22 14:47:42 +0100

    Remove static keyword from methods in AssetStorage class

--- AssetTrackingWithEF/Services/AssetStorage.cs
+++ AssetTrackingWithEF/Services/AssetStorage.cs
@@ -4,32 +4,32 @@ namespace AssetTrackingWithEF.Services
 {
     public class AssetStorage
     {       
-        public static List<Asset> LoadAssets()
+        public List<Asset> LoadAssets()
         {
             using var context = new MyDbContext();
             context.Assets.ToList();
             return context.Assets.ToList();
         }
 
-        public static List<string> GetCategories()
+        public List<string> GetCategories()
         {
             using var context = new MyDbContext();
             return context.Categories.Select(x => x.CategoryName).ToList();
         }
 
-        public static List<string> GetOfficeLocations()
+        public List<string> GetOfficeLocations()
         {
             using var context = new MyDbContext();
             return context.Offices.Select(o => o.OfficeLocation).ToList();
         }
 
-        public static void SaveAsset(Asset asset)
+        public void SaveAsset(Asset asset)
         {
             using var context = new MyDbContext();
             context.Assets.Add(asset);
         }
 
-        public static void DeleteAsset(Asset asset) 
+        public void DeleteAsset(Asset asset) 
         {
             using var context = new MyDbContext();
             context.Assets.Remove(asset);
